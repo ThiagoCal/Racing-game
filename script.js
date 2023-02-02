c0373c1fa68ec7fdbaced7554735d26a2fca03e0
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-02-02 12:20:07 +0200

    Changed the boat position

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let containerBox = document.getElementById("container")
 let boxDiv = document.getElementById("animate")
-let initialPositionLeft = 645;
-let inintialPositionTop = 105;
+let initialPositionLeft = 90;
+let inintialPositionTop = 14
+let animationInterval = setInterval(animationBox, Math.floor((Math.random()*5)+Math.random(0,60)))
+
 function animationBox(){
-    inintialPositionTop += 0.5
-    initialPositionLeft -= 1
-    boxDiv.style.left = initialPositionLeft + "px";
-    boxDiv.style.top = inintialPositionTop + "px";
+    inintialPositionTop +=0.07
+    initialPositionLeft -= 0.14
+    boxDiv.style.left = initialPositionLeft + "vh";
+    boxDiv.style.top = inintialPositionTop + "vh";
     // boxDiv.style.transform = "rotate("+initialPosition+"deg)"
     // boxDiv.style.width = initialPosition + "px"
     // console.log(initialPosition)
-    if(initialPositionLeft === 40){
+    if(initialPositionLeft < 5.6){
         console.log("hello")
         clearInterval(animationInterval);
     }
 }
 
-let animationInterval = setInterval(animationBox, Math.floor((Math.random()*5)+Math.random(0,60)))
 
 
 
